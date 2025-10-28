1331cb2936e57ef3bc64199e69bb29a5575c37ba
sekiyuusuke <golfseki39@gmail.com>
2025-10-28 17:42:19 +0900

    astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/"; // ★ 1. Netlifyアダプタをインポート
+import netlify from "@astrojs/netlify"; // ★ 1. Netlifyアダプタをインポート
 
 import partytown from "@astrojs/partytown";
 
